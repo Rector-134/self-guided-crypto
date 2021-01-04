75967820a48d90f6bbf9309dff5989db02907799
Rector-134 <70325899+Rector-134@users.noreply.github.com>
2021-01-04 22:34:17 +0000

    Delete set1ch1.py

--- set1ch1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import codecs
-
-hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-b64 = codecs.encode(codecs.decode(hex_string, 'hex'), 'base64').decode()
-print (b64)
